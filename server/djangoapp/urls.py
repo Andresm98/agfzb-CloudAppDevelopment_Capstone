bc180bfa9c795eb23a9f034a1fcd87224edcf939
Andresm98 <andresm17u7@hotmail.com>
2022-11-23 13:31:33 -0500

    Lab 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,9 +9,15 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path('/', views.index, name='index'),
+
     # path for about view
 
+    path('about/', views.about, name='about'),
+
     # path for contact us view
+
+    path('contact/', views.contact, name='contact'),
 
     # path for registration
 
